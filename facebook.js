55519a33607b2db872a888951c9661695bd00ebe
Billal <dipukhan8826@gmail.com>
2021-05-25 16:28:38 +0600

    Al-Jazeera facebook cloning page- added before responsive

---
+++ facebook.js
@@ -0,0 +1,82 @@
+// Type Writer Effect --start
+
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
+  // Type Writer Effect ---end
+
+// Vertical tab js --start
+
+function _class(name){
+    return document.getElementsByClassName(name);
+  }
+  
+  let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+  
+  for(let i=0;i<tabPanes.length;i++){
+    tabPanes[i].addEventListener("click",function(){
+      _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+      tabPanes[i].classList.add("active");
+      
+      _class("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;
+      
+      _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+      _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+      
+    });
+  }
+// Vertical tab js --end
